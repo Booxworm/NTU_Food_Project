65d83028cda02198ce9b214c6e859563d350f534
Wonn Jen <lee.wonnjen@gmail.com>
2018-10-26 16:01:39 +0800

    Update db.py

--- resources/db.py
+++ resources/db.py
@@ -1,1 +1,10 @@
-hall
+canteens = [
+    'can1' : {
+        'coords' : (x,y),
+        'food' : {
+            'food_name' : 'price'
+        },
+        'rank' : int,
+        'opening_hours' : (0800,2300)
+    }
+]
