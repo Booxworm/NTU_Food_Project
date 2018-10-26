cf73449b43ce2822c7ad256c1217434b7c136b22
Wonn Jen <lee.wonnjen@gmail.com>
2018-10-26 18:32:03 +0800

    Update main.py

--- gui.py
+++ gui.py
@@ -1,36 +1,43 @@
 import pygame
 
-# Inits pygame
-pygame.init()
+def getCoordsClick():
+    # Inits pygame
+    pygame.init()
 
-# Inits clock to limit FPS
-clock = pygame.time.Clock()
+    # Inits clock to limit FPS
+    clock = pygame.time.Clock()
 
-# Creates screen
-map = pygame.image.load("./resources/ntuMap.jpeg")
-mapSize = (1310,1600)
-scaledSize = (int(mapSize[0]/3), int(mapSize[1]/3))
-map = pygame.transform.scale(map, scaledSize)
-screen = pygame.display.set_mode(scaledSize)
+    # Creates screen
+    map = pygame.image.load("./resources/ntuMap.jpeg")
+    mapSize = (1310,1600)
+    scaledSize = (int(mapSize[0]/3), int(mapSize[1]/3))
+    map = pygame.transform.scale(map, scaledSize)
+    screen = pygame.display.set_mode(scaledSize)
 
-# Game loop
-running = True
-while running:
-    # Event handler
-    for event in pygame.event.get():
-        # Handles quit event
-        if event.type == pygame.QUIT:
-            running = False
-        # handle MOUSEBUTTONUP
-        if event.type == pygame.MOUSEBUTTONUP:
-            print(pygame.mouse.get_pos())
+    # Co-ordinates of user click
+    coords = False
 
-    # Display map
-    screen.blit(map,(0,0))
-    pygame.display.flip()
+    # Game loop
+    running = True
+    while running:
+        # Event handler
+        for event in pygame.event.get():
+            # Handles quit event
+            if event.type == pygame.QUIT:
+                running = False
+            # handle MOUSEBUTTONUP
+            if event.type == pygame.MOUSEBUTTONUP:
+                coords = pygame.mouse.get_pos()
+                running = False
 
-    # Set to 60 FPS
-    clock.tick(60)
+        # Display map
+        screen.blit(map,(0,0))
+        pygame.display.flip()
 
-# Closes pygame
-pygame.quit()
+        # Set to 60 FPS
+        clock.tick(60)
+
+    # Closes pygame
+    pygame.quit()
+
+    return coords
--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-def findCanteen(criteria):
-    pass
+import gui
 
+# Gets user input, returns a number based on the option selected
+def getInput(msg, options):
+    # Displays input message and options
+    print(msg)
+    for i in range(len(options)):
+        print("{}. {}".format(i+1, options[i]))
+    print("{}. Exit".format(len(options)+1))
+
+    # Gets and checks user input
+    userInput = input()
+    print()
+    while not userInput.isdigit() or int(userInput) not in range(1,len(options)+2):
+        userInput = input("Give a valid input ({}): ".format('/'.join(range(1,len(options)+2))))
+    return userInput
+
+# Finds a canteen based on criteria
+def findCanteen(criteria):
+    if criteria == 'Distance':
+        print(gui.getCoordsClick())
+    return True
+
+# Updates information about a canteen
 def updateCanteen(canteen, params):
     pass
 
-print("Welcome to NTU F&B Recommendations!\n\
-       What would you like to do?\n\
-       1. Find a canteen (based on distance, price, rank)\n\
-       2. Update information about a canteen\n\
-       3. Exit")
+# Variable declarations
+choiceList = ["Find a canteen (based on distance, price, rank)",\
+              "Update information about a canteen"]
+criteriaList = ['Distance', 'Price', 'Rank']
 
-user_input = input()
-while not user_input.isdigit() or int(user_input) not in range(1,4):
-    user_input = input("Give a valid input (1/2/3)")
+# Get choice
+choice = getInput("Welcome to NTU F&B Recommendations!\nWhat would you like to do?", choiceList)
 
+# Finds the canteen based on criteria
+if choice == '1':
+    criteria = getInput("What is the criteria you want to use?", criteriaList)
+
+    # Exits program
+    if criteria == str(len(criteriaList)+1):
+        pass
+
+    # Find canteen based on criteria
+    else:
+        canteen = findCanteen(criteriaList[int(criteria)-1])
+        print(canteen)
+
+# Updates canteen
+elif choice == '2':
+    pass
+
+# End program
 print("Thanks")
