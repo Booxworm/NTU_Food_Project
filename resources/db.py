c1b6927ff984ffebc5a0cb095475869803419865
VinciHu <huwenqi614@gmail.com>
2018-10-29 12:12:29 +0800

    Add coords in database of canteen

--- resources/db.py
+++ resources/db.py
@@ -1,10 +1,101 @@
 canteens = [
-    'can1' : {
-        'coords' : (x,y),
-        'food' : {
-            'food_name' : 'price'
+    'default' : { 
+        'coords' : (436,566), #a tuple as value
+        'food' : { # a dictionary as value
+            'food_name1' : 2.00, # price1, float
+            'food_name2' : 8.00,
         },
-        'rank' : int,
-        'opening_hours' : (0800,2300)
-    }
-]
+        'rank' : 11, #from 1 to 10
+        'opening_hours' : (0800,2300) # the range of opening hours
+    },
+    'can1' : { 
+        'coords' : (291,311), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 1, 
+        'opening_hours' : (0700,2100)
+    },    
+    'can2' : { 
+        'coords' : (271,264), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 2, 
+        'opening_hours' : (0700,2100)
+    },
+    'can9' : { 
+        'coords' : (279,141), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 3, 
+        'opening_hours' : (0700,2100)
+    },    
+    'can11' : { 
+        'coords' : (303,68), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 4, 
+        'opening_hours' : (0700,2100)
+    },    
+    'can13' : { 
+        'coords' : (144,162), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 5, 
+        'opening_hours' : (0700,2100)
+    },    
+    'can14' : { 
+        'coords' : (178,126), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 6, 
+        'opening_hours' : (0700,2100)
+    },
+    'can16' : { 
+        'coords' : (142,199), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 7, 
+        'opening_hours' : (0700,2100)
+    },    
+    'canA' : { 
+        'coords' : (113,298), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 8, 
+        'opening_hours' : (0700,2100) #ph and weekends closed
+    },        
+    'canB' : { 
+        'coords' : (178,431), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 9, 
+        'opening_hours' : (0700,2100) #Sundays closed
+    },   
+    'QuadCafe' : { 
+        'coords' : (104,362), 
+        'food' : { 
+            'food_name1' : 2.00,
+            'food_name2' : 8.00,
+        },
+        'rank' : 10, 
+        'opening_hours' : (0700,2100) #ph and Sundays closed
+    },
+    ]
