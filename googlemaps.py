23d377c9892d997cb3be46f4984f96a5da22c4a1
Shivani Kannan <44492942+nicodiangel0@users.noreply.github.com>
2018-11-11 08:38:01 +0530

    Update googlemaps.py

--- googlemaps.py
+++ googlemaps.py
@@ -1,7 +1,7 @@
 import googlemaps
 from math import sin, cos, sqrt, atan2, radians
 
-                                     
+"""                                    
 #lat and lon of the mouse click
 import arcpy  
 import pythonaddins  
@@ -18,47 +18,13 @@ class XYTool(object):
             pt=arcpy.PointGeometry(arcpy.Point(x,y))  
             #ptfeat=arcpy.management.CopyFeatures(pt,r"in_memory\pt")  
             print x,y  
-            pythonaddins.MessageBox(str(x) + ", " + str(y), 'Coordinates', 0)  
+            pythonaddins.MessageBox(str(x) + ", " + str(y), 'Coordinates', 0)  """
  
 #calc the dist between the user loc and the canteens
 
 # approximate radius of earth in km
 R = 6373.0
-"""lat and lon of each canteen
-can1=(1.346763,103.686018)
-can2=(1.348434,103.685535)
-can9=(1.352515,103.685760)
-can11=(1.355156,103.686944)
-can13=(1.351925,103.681628)
-can14=(1.352961,103.682432)
-can16=(1.350618,103.681652)
-koufu=(1.342642,103.682791)
-NS=(1.347243,103.680236)
-quad=(1.345193,103.680549)"""
-"""
-lat=[]
-lat[0]=1.346763
-lat[1]=1.348434
-lat[2]=1.352515
-lat[3]=1.355156
-lat[4]=1.351925
-lat[5]=1.352961
-lat[6]=1.350618
-lat[7]=1.342642
-lat[8]=1.347243
-lat[9]=1.345193
 
-lon=[]
-lon[0]=103.686018
-lon[1]=103.685535
-lon[2]=103.685760
-lon[3]=103.686944
-lon[4]=103.681628
-lon[5]=103.682432
-lon[6]=103.681652
-lon[7]=103.682791
-lon[8]=103.680236
-lon[9]=103.680549"""
 
 def dist(a,b):
 
